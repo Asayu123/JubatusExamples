a62c1839e5d9a47cce8f8d7a1d4392255cb43626
Asayu123 <9923464+Asayu123@users.noreply.github.com>
2018-11-05 19:02:55 +0900

    refactor: Remove dependency to specific service in k fold helper function.

--- client/main/mail_classifier/validation.py
+++ client/main/mail_classifier/validation.py
@@ -1,29 +1,27 @@
-from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics.classification import classification_report
 
-from client.modules.wrapper import ExtendedClassifier
+from jubakit.classifier import Classifier
+
+from client.modules.helpers import k_fold_validation
 from client.settings import MAIL_CLASSIFIER_HOST, MAIL_CLASSIFIER_PORT
 from client.main.mail_classifier.data.loader import load_mails
 
 # Load a CSV file.
 dataset = load_mails()
 
-# Setting up a K-fold validation parameters.
-labels = list(dataset.get_labels())
-skf = StratifiedKFold(n_splits=10)
-train_test_indices = skf.split(labels, labels)
-
 # Connect to an existing Jubatus service.
-classifier = ExtendedClassifier(MAIL_CLASSIFIER_HOST, MAIL_CLASSIFIER_PORT)
+classifier = Classifier(MAIL_CLASSIFIER_HOST, MAIL_CLASSIFIER_PORT)
 
 # Execute training with K-fold validation.
-true_labels, predicted_labels = classifier.k_fold_validation(
-    dataset=dataset, train_test_indices=train_test_indices
+true_labels, predicted_labels = k_fold_validation(
+    service=classifier, dataset=dataset,
+    n_splits=10,
 )
+
+# Show a classification report.
+print(' === Summary of K-fold validation === ')
+print(classification_report(true_labels, predicted_labels))
 
 # Stop and clear the classifier.
 classifier.stop()
 classifier.clear()
-
-# Show a classification report.
-print(classification_report(true_labels, predicted_labels))
